38b6491da1831e760e9546d48cca966bcbd22ef0
kkjj2485 <kkjj2485@googlemail.com>
2015-12-24 13:38:43 +0800

    ga form added

---
+++ app/assets/javascripts/new.js
@@ -0,0 +1,19 @@
+(<span style="color: #008000; font-weight: bold;">function</span>($) {
+$(<span style="color: #007020;">document</span>).ready(<span style="color: #008000; font-weight: bold;">function</span>() {
+	$(<span style="background-color: #fff0f0;">':input'</span>).blur(<span style="color: #008000; font-weight: bold;">function</span> () {
+		<span style="color: #008000; font-weight: bold;">if</span>($(<span style="color: #008000; font-weight: bold;">this</span>).val().length <span style="color: #303030;">&gt;</span> <span style="color: #0000d0; font-weight: bold;">0</span>) {
+			_gaq.push([<span style="background-color: #fff0f0;">'_trackEvent'</span>, <span style="background-color: #fff0f0;">'name'</span>, <span style="background-color: #fff0f0;">'completed'</span>, $(<span style="color: #008000; font-weight: bold;">this</span>).attr(<span style="background-color: #fff0f0;">'name'</span>)]);
+		}
+		<span style="color: #008000; font-weight: bold;">else</span> {
+			_gaq.push([<span style="background-color: #fff0f0;">'_trackEvent'</span>, <span style="background-color: #fff0f0;">'email'</span>, <span style="background-color: #fff0f0;">'skipped'</span>, $(<span style="color: #008000; font-weight: bold;">this</span>).attr(<span style="background-color: #fff0f0;">'name'</span>)]);
+	
+		}
+
+		<span style="color: #008000; font-weight: bold;">else</span> {
+			_gaq.push([<span style="background-color: #fff0f0;">'_trackEvent'</span>, <span style="background-color: #fff0f0;">'phone'</span>, <span style="background-color: #fff0f0;">'skipped'</span>, $(<span style="color: #008000; font-weight: bold;">this</span>).attr(<span style="background-color: #fff0f0;">'name'</span>)]);
+	
+		}
+
+	);
+});
+})(jQuery);
